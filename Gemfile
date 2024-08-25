7576cb99271a18b67d903464d0f4e083f428ea00
gokutheengineer <engineerzubizaretta@gmail.com>
2024-08-25 13:02:18 +0300

    feat: mock interactions

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ source "https://rubygems.org"
 
 # gem "rails"
 gem "coinbase", "~> 4.2.3"
+gem "certified"
+gem "vcr"
+gem "webmock"
--- main.rb
+++ main.rb
@@ -1,7 +1,13 @@
 require 'coinbase/wallet'
 require 'yaml'
 require 'certified'
+require 'vcr'
+require 'webmock'
 
+VCR.configure do |config|
+  config.cassette_library_dir = 'casettes'
+  config.hook_into :webmock
+end
 
 # Load config
 config = YAML.load_file('config.yml')
@@ -15,15 +21,20 @@ puts "Using configuration for #{username}, with API key #{api_key} and secret #{
 # Create a new client
 coinbaseClient = Coinbase::Wallet::Client.new(api_key: api_key, api_secret:api_secret)
 
-begin
-  account = coinbaseClient.accounts
-  account.each do |account|
-    puts "Balance: #{account.balance.amount} #{account.balance.currency}"
+
+# Use VCR to record/replay API calls to Coinbase
+VCR.use_cassette('coinbase_accounts') do
+  begin
+    account = coinbaseClient.accounts
+    account.each do |account|
+      puts "Balance: #{account.balance.amount} #{account.balance.currency}"
+    end
+
+  rescue Coinbase::Wallet::APIError  => e
+    puts "Coinbase wallet error: #{e.message}"
+
+  rescue OpenSSL::SSL::SSLError => ssl_error
+    puts "SSL error: #{ssl_error.message}"
   end
 
-rescue Coinbase::Wallet::APIError  => e
-  puts "Coinbase wallet error: #{e.message}"
-
-rescue OpenSSL::SSL::SSLError => ssl_error
-  puts "SSL error: #{ssl_error.message}"
 end
